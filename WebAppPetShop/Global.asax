3a9786bbe82701e6ad8e8e8e8d2943423c6b0661
Pablo Joerke <joerke08@outlook.com>
2018-02-21 21:17:52 -0300

    v1

---
+++ WebAppPetShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppPetShop.MvcApplication" Language="C#" %>
---
+++ WebAppPetShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppPetShop.Startup))]
+namespace WebAppPetShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
